3b02273196bfff5f9a92c965ddca997c857f5466
Radin <radinerfanfar@gmail.com>
2023-10-23 09:49:18 +0330

    utils.dart created

--- lib/PomodoroScreen.dart
+++ lib/PomodoroScreen.dart
@@ -5,6 +5,12 @@ class PomodoroScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return   Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.redAccent,
+        title: Text('Pomodox', style: ,),
+      ),
+    );
   }
 }
