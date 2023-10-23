a5dec42490a79e2faf83bfb61257ac9acf66746d
Radin <radinerfanfar@gmail.com>
2023-10-23 20:48:42 +0330

    the core of pomodox

--- lib/timerservice.dart
+++ lib/timerservice.dart
@@ -7,10 +7,24 @@ class TimerService extends ChangeNotifier {
 
   //TimerService({required this.timer});
   // @override       //* havaset be in tike bashe
-
-  late Timer timer;
+  Timer timer = Timer.periodic(Duration(seconds: 1), (timer) {});
   double currentDuration = 1500;
   double selectTime = 1500;
+  bool timerPlaying = false;
+
+  void start() {
+    timerPlaying = true;
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      currentDuration--;
+      notifyListeners();
+    });
+  }
+
+  void pause() {
+    timer.cancel();
+    timerPlaying = false;
+    notifyListeners();
+  }
 
   void selectedTime(double seconds) {
     selectTime = seconds;
--- lib/widgets/timecontroller.dart
+++ lib/widgets/timecontroller.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pomodox/timerservice.dart';
+import 'package:provider/provider.dart';
 
 class TimeController extends StatefulWidget {
   const TimeController({super.key});
@@ -10,20 +12,30 @@ class TimeController extends StatefulWidget {
 class _TimeControllerState extends State<TimeController> {
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<TimerService>(context);
     return Container(
-      width: 85,
-      height: 85,
-      decoration:
-          const BoxDecoration(color: Colors.black26, shape: BoxShape.circle),
-      child: IconButton(
-          onPressed: () {},
-          icon: const Center(
-            child: Icon(
-              Icons.pause,
-              color: Colors.white,
-              size: 50,
-            ),
-          )),
-    );
+        width: 85,
+        height: 85,
+        decoration:
+            const BoxDecoration(color: Colors.black26, shape: BoxShape.circle),
+        child: IconButton(
+          onPressed: () {
+            provider.timerPlaying
+                ? Provider.of<TimerService>(context, listen: false).pause()
+                : Provider.of<TimerService>(context, listen: false).start();
+          },
+          icon: Center(
+              child: provider.timerPlaying
+                  ? const Icon(
+                      Icons.pause_outlined,
+                      color: Colors.white,
+                      size: 50,
+                    )
+                  : const Icon(
+                      Icons.play_arrow_rounded,
+                      color: Colors.white,
+                      size: 50,
+                    )),
+        ));
   }
 }
