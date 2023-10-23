724d87db7c9af0f635a14a60e06ac4fb1c0ae20a
Radin <radinerfanfar@gmail.com>
2023-10-23 11:15:44 +0330

    /widgets/timercard.dart created

--- lib/PomodoroScreen.dart
+++ lib/PomodoroScreen.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:pomodox/utils.dart';
 
 class PomodoroScreen extends StatelessWidget {
   const PomodoroScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.redAccent,
-        title: Text('Pomodox', style: ,),
+        title: Text(
+          'Pomodox',
+          style: textStyle(24, Colors.white54, FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.refresh,
+                color: Colors.white54,
+                size: 24,
+              ))
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              alignment: Alignment.center,
+              child: Column(
+                children: [],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
