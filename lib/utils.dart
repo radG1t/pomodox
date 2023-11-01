03301fc9508589d3aca151568b9778fb0846b215
Radin <radinerfanfar@gmail.com>
2023-11-01 11:02:21 +0330

    working on random sentence

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:math';
 
 TextStyle textStyle(double size, Color color, FontWeight fw) {
   return GoogleFonts.lato(fontSize: size, fontWeight: fw, color: color);
@@ -29,3 +30,40 @@ Color rednderColor(String currentState) {
     return Colors.lightBlueAccent;
   }
 }
+
+String workRandomSentence() {
+  List<String> sentences = [
+    'Stay focused',
+    'make it happen!',
+    'Dream big, work hard',
+    'Every small step counts',
+    'Keep moving forward.'
+  ];
+  Random random = Random();
+  int index = random.nextInt(sentences.length);
+  return sentences[index];
+}
+
+String breakRandomSentence() {
+  List<String> sentences = [
+    'Rest your mind, refresh your focus',
+    'Get rest',
+    'Take a deep breath',
+    'Relax and recharge!',
+    'Give yourself a break,You deserve it;)',
+    'Disconnect and recharge',
+  ];
+  Random random = Random();
+  int index = random.nextInt(sentences.length);
+  return sentences[index];
+}
+
+class getRandomSentence extends StatelessWidget {
+  const getRandomSentence({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(workRandomSentence());
+  }
+}
+// if focus page this randoms else those randoms
