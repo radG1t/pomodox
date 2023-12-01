8dae15b3c25a73d9a6b03e88dbf7d8c05f18ddfe
Radin <radinerfanfar@gmail.com>
2023-12-01 18:37:49 +0330

    update animation.dart

--- lib/widgets/animation.dart
+++ lib/widgets/animation.dart
@@ -10,12 +10,23 @@ class animation extends StatelessWidget {
     final provider = Provider.of<TimerService>(context);
     String animationName = provider.currentState == "Focus" ? "work" : "rest";
     bool animationRepeat = provider.currentState == "Focus" ? false : true;
-    return Lottie.asset(
-      'assets/$animationName.json',
-      width: 300,
-      height: 300,
-      fit: BoxFit.fill,
-      repeat: animationRepeat,
+    double animationHeight = provider.currentState == "Focus" ? 30 : 0;
+    return Column(
+      children: [
+        SizedBox(
+          height: animationHeight,
+        ),
+        Lottie.asset(
+          'assets/$animationName.json',
+          width: 300,
+          height: 300,
+          fit: BoxFit.fill,
+          repeat: animationRepeat,
+        ),
+        SizedBox(
+          height: animationHeight,
+        ),
+      ],
     );
   }
 }
